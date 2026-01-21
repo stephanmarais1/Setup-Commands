8bd76f47a417f45b1df4f8b5dd4e9001ab901a00
Stephan Marais <105236583+stephanmarais1@users.noreply.github.com>
2026-01-21 17:43:34 +0200

    Update Commands.bash

--- fsharp/Commands.bash
+++ fsharp/Commands.bash
@@ -1,8 +1,26 @@
 #project setup
-ProjectName="<Name here>"
+
+ProjectName="MyAlgo"
+
+#create a solution file if this is not part of an existing repo
+git init
+dotnet new gitignore
+git add .gitignore
+git commit -m "Add .gitignore file"
+dotnet new sln --name $ProjectName
 
 #create initial FSharp project structures
 dotnet new install BenchmarkDotnet.Templates
-dotnet new classlib -o $ProjectName
-dotnet new benchmark -language F# -o "{$ProjectName}.Benchmark"
-dotnet new xunit -language F# -o "{$ProjectName}.Tests"
+dotnet new classlib --language F# -o $ProjectName
+dotnet new benchmark --language F# -o "$ProjectName.Benchmark"
+dotnet new xunit --language F# -o "$ProjectName.Tests"
+
+dotnet sln add $ProjectName
+dotnet sln add "$ProjectName.Benchmark"
+dotnet sln add "$ProjectName.Tests"
+
+
+#more useful commands:
+
+#add all fsharp projects to the solution
+dotnet sln add $(ls **/*.fsproj)
